babd0e013444a7b3fabf770fc43d613b0d5e9ed9
Michael Klishin <michael@defprotocol.org>
2013-04-09 02:37:06 +0400

    Expm packaging

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ MoneyEx is an Elixir library for working with monetary amounts and currencies,
 heavily inspired by [Joda Money](http://joda-money.sourceforge.net/) and [ClojureWerkz Money](https://github.com/clojurewerkz/money).
 
 
+## Dependency Information
+
+MoneyEx is [released to expm.co](http://expm.co/money).
+
+
 ## License
 
 Released under the Apache License, Version 2.0.
---
+++ package.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(name: "money", description: "Library for working with monetary amounts and currencies",
+                 version: "1.0.0-alpha1", keywords: ["money"], 
+                 maintainers: [[name: "Michael S. Klishin", 
+                                email: "michael@defprotocol.org"]],
+                 repositories: [[github: "michaelklishin/money.ex"]])
+    
