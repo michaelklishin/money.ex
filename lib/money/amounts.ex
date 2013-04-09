3ebe23d79bc6cabd8f34659f3f9e33b5247f777b
Michael Klishin <michael@defprotocol.org>
2013-04-09 20:39:58 +0400

    Verify that currencies are the same

--- lib/money/amounts.ex
+++ lib/money/amounts.ex
@@ -57,7 +57,7 @@ defmodule Money.Amounts do
   end
 
   def compare(a, b) do
-    # TODO: raise on different currencies
+    check_that_currencies_are_identical(a, b)
 
     :decimal.compare(a.amount, b.amount)
   end
@@ -67,40 +67,40 @@ defmodule Money.Amounts do
   end
 
   def greater_than?(a, b) do
-    # TODO: raise on different currencies
+    check_that_currencies_are_identical(a, b)
 
     :decimal.compare(a.amount, b.amount) == 1
   end
 
   def less_than?(a, b) do
-    # TODO: raise on different currencies
+    check_that_currencies_are_identical(a, b)
 
     :decimal.compare(a.amount, b.amount) == -1
   end
 
   def greater_than_or_equals?(a, b) do
-    # TODO: raise on different currencies
+    check_that_currencies_are_identical(a, b)
 
     c = :decimal.compare(a.amount, b.amount)
     c == 1 || c == 0
   end
 
   def less_than_or_equals?(a, b) do
-    # TODO: raise on different currencies
+    check_that_currencies_are_identical(a, b)
 
     c = :decimal.compare(a.amount, b.amount)
     c == -1 || c == 0
   end
 
   def add(a, b) do
-    # TODO: raise on different currencies
+    check_that_currencies_are_identical(a, b)
 
     x = :decimal.add(a.amount, b.amount)
     Money.new(currency_unit: a.currency_unit, amount: x)
   end
 
   def add(a, b, context) do
-    # TODO: raise on different currencies
+    check_that_currencies_are_identical(a, b)
 
     x = :decimal.add(a.amount, b.amount, context)
     Money.new(currency_unit: a.currency_unit, amount: x)
@@ -110,14 +110,14 @@ defmodule Money.Amounts do
   # TODO: plus-minor
 
   def subtract(a, b) do
-    # TODO: raise on different currencies
+    check_that_currencies_are_identical(a, b)
 
     x = :decimal.subtract(a.amount, b.amount)
     Money.new(currency_unit: a.currency_unit, amount: x)
   end
 
   def subtract(a, b, context) do
-    # TODO: raise on different currencies
+    check_that_currencies_are_identical(a, b)
 
     x = :decimal.subtract(a.amount, b.amount, context)
     Money.new(currency_unit: a.currency_unit, amount: x)
@@ -126,28 +126,28 @@ defmodule Money.Amounts do
   # TODO: total
 
   def multiply(a, b) do
-    # TODO: raise on different currencies
+    check_that_currencies_are_identical(a, b)
 
     x = :decimal.multiply(a.amount, b)
     Money.new(currency_unit: a.currency_unit, amount: x)
   end
 
   def multiply(a, b, context) do
-    # TODO: raise on different currencies
+    check_that_currencies_are_identical(a, b)
 
     x = :decimal.multiply(a.amount, b, context)
     Money.new(currency_unit: a.currency_unit, amount: x)
   end
 
   def divide(a, b) do
-    # TODO: raise on different currencies
+    check_that_currencies_are_identical(a, b)
 
     x = :decimal.divide(a.amount, b)
     Money.new(currency_unit: a.currency_unit, amount: x)
   end
 
   def divide(a, b, context) do
-    # TODO: raise on different currencies
+    check_that_currencies_are_identical(a, b)
 
     x = :decimal.divide(a.amount, b, context)
     Money.new(currency_unit: a.currency_unit, amount: x)
@@ -162,5 +162,15 @@ defmodule Money.Amounts do
   defp amount_in_minor_units_to_decimal(amount, decimal_places) do
     s = :io_lib.format("~wE-~w", [amount, decimal_places]) |> iolist_to_binary
     :decimal_conv.number(s)
+  end
+
+  defp check_that_currencies_are_identical(a, b) do
+    if !(a.currency_unit.code == b.currency_unit.code) do
+      s = :io_lib.format("currencies are not the same: ~w, ~w", [a.currency_unit.code, b.currency_unit.code]) |> iolist_to_binary
+
+      raise ArgumentError.new(message: s)
+      else
+        true
+    end 
   end
 end
